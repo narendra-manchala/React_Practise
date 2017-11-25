062cfe3b3b3f5be18644a298272f0fa8fb5981f6
narendra67 <mvnarendrareddy@gmail.com>
2017-11-25 20:07:56 +0530

    Added a state less function and a function with class

--- component_intro/index.js
+++ component_intro/index.js
@@ -1,8 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+/**
+ * component as a class
+ */
 
-class IngredientsList extends React.Component {
+
+/*class IngredientsList extends React.Component {
 
     renderListItem(ingredient, i) {
         return React.createElement("li", { key: i }, ingredient)
@@ -14,7 +18,21 @@ class IngredientsList extends React.Component {
         )
     }
 
-}
+}*/
+
+/**
+ * Component as a stateless function
+ */
+
+/*const IngredientsList = ({items}) =>
+
+    React.createElement("ul", {className:"ingredients"},
+        items.map((ingredient, i) =>
+            React.createElement("li", {key: i}, ingredient)
+        )
+    )
+
+
 
 
 const items = [
@@ -28,5 +46,35 @@ const items = [
 
 ReactDOM.render(
     React.createElement(IngredientsList, {items}, null),
+    document.getElementById('container')
+)*/
+
+
+/**
+ *factories example
+ */
+
+const { render } = ReactDOM;
+
+const IngredientsList = ({ list }) =>
+    React.createElement('ul', null,
+        list.map((ingredient, i) =>
+            React.createElement('li', {key: i}, ingredient)
+        )
+    )
+
+const Ingredients = React.createFactory(IngredientsList)
+
+const list = [
+    "1 lb Salmon",
+    "1 cup Pine Nuts",
+    "2 cups Butter Lettuce",
+    "1 Yellow Squash",
+    "1/2 cup Olive Oil",
+    "3 cloves of Garlic"
+]
+
+render(
+    Ingredients({list}),
     document.getElementById('container')
 )
